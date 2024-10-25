cd67e7be30f7c541aab22f26caf19f9a286fe051
mizuki44 <rat.raat.44@gmail.com>
2024-10-25 19:54:01 +0900

    レイアウト修正

--- src/resources/views/shop_detail.blade.php
+++ src/resources/views/shop_detail.blade.php
@@ -9,7 +9,7 @@
 
 
 <main>
-    @section('content')
+@section('content')
     <script>
         $(function() {
             $(document).on('change', '#date', function(e) {
@@ -29,153 +29,148 @@
             });
         });
     </script>
-    <div class="detail_page">
-
-        <!-- 店の詳細表示 -->
-        <div class="shop_detail">
-            <!-- 店名 -->
-            <div class="shop_name">
-                <button class="shop_trans" onclick="location.href='{{url('/')}}'">
-                    ＜</button>
-                <h1 class="shop_name3">{{ $shop['name'] }}</h1>
-            </div>
-            <!-- 画像 -->
-            <div class="image_content">
-                <img class="image_content2" src="{{ $shop['image_url'] }}">
-            </div>
-            <!-- 分類 -->
-            <div class="classification">
-                <span>#{{ $shop['area']['name'] }}</span>
-                <span>#{{ $shop['genre']['name'] }}</span>
-            </div>
-            <!-- 説明 -->
-            <div class="classification">
-                <p>{{ $shop['description']}}</p>
-                <p class="review__information">全ての口コミ情報</p>
-            </div>
-            <!-- 口コミ表示 -->
-            @if( Auth::check() )
-            @if( is_null($my_review) )
-            <div class="review">
-                <a href="{{ url('/review/add/'.$shop['id']) }}" class="review_button_add">口コミを投稿する</a>
-            </div>
-            @endif
-            @endif
-            @foreach($reviews as $review)
-            @if($review->user_id == Auth::user()->id)
-            <hr>
-            <div class="button_flex">
-                <div class="review_button_flex">
-                    <a href="{{ url('/review/edit/'.$shop['id']) }}" class="review__edit__button">口コミを編集</a>
-                </div>
-                <div class="review_button_flex">
-                    <form method="POST" action="{{ url('/review/delete') }}">
-                        @csrf
-                        <input type="hidden" name="shop_id" value="{{ $shop['id'] }}">
-                        <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                        <button type="submit" class="review__delete__button">口コミを削除</button>
-                    </form>
-                </div>
-            </div>
-            @endif
-            <div class="star">
-                @for ($counter = 0; $counter < 5; $counter++) <span class="{{ $counter < $review['star'] ? 'text-blue-600' : 'text-gray-200'}}">★</span>
-                    @endfor
-            </div>
-            <p>{{$review['comment']}}</p>
-            <div class="image_content">
-                <img class="review_image" src="{{ $review['image_url'] }}" />
-            </div>
-            <hr>
-            @endforeach
-
+<div class="detail_page">
+    <!-- 店の詳細表示 -->
+    <div class="shop_detail">
+        <!-- 店名 -->
+        <div class="shop_name__flex">
+            <button class="shop_trans" onclick="location.href='{{url('/')}}'">
+                ＜</button>
+            <h1 class="shop_name">{{ $shop['name'] }}</h1>
         </div>
-
-        <!-- 予約 -->
-        <div class="reserve_content">
-            <h1 class="reserve">予約</h1>
-            <p class="reserve_text"></p>
-            @if (count($errors) > 0)
-            <p class='error_message'>入力に問題があります</p>
-            @endif
-            <form action="{{ route('reserve') }}" method="POST">
-                @csrf
-                <input type="hidden" name="shop_id" value="{{ $shop['id'] }}">
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <div class="date">
-                    <input class="date" type="date" id="date" name="date" value="" min="{{ $tomorrow }}" />
-                </div>
-                @error('date')
-                <p class='error_message'>{{$message}}</p>
-                @enderror
-
-                <!-- 時間のドロップダウン -->
-                <div class="drop_time">
-                    <select name="time" class="drop_time" id="time">
-                        <optgroup label="lunch">
-                            <option value="" selected hidden>選択してください</option>
-                            <option value="11:00">11:00</option>
-                            <option value="11:30">11:30</option>
-                            <option value="12:00">12:00</option>
-                            <option value="12:30">12:30</option>
-                            <option value="13:00">13:00</option>
-                        </optgroup>
-                        <optgroup label="dinner">
-                            <option value="18:00">18:00</option>
-                            <option value="18:30">18:30</option>
-                            <option value="19:00">19:00</option>
-                            <option value="19:30">19:30</option>
-                            <option value="20:00">20:00</option>
-                        </optgroup>
-                    </select>
-                </div>
-                @error('time')
-                <p class='error_message'>{{$message}}</p>
-                @enderror
-                <!-- 人数のドロップダウン -->
-                <div class="drop_number">
-                    <select name="number" class="drop_number" id="number">
-                        <option value="" selected hidden>選択してください</option>
-                        <option value="1">1人</option>
-                        <option value="2">2人</option>
-                        <option value="3">3人</option>
-                        <option value="4">4人</option>
-                        <option value="5">5人</option>
-                        <option value="6">6人</option>
-                        <option value="7">7人</option>
-                        <option value="8">8人</option>
-                        <option value="9">9人</option>
-                        <option value="10">10人</option>
-                    </select>
-                </div>
-                @error('number')
-                <p class='error_message'>{{$message}}</p>
-                @enderror
-
-                <button type="submit" class="reserve_button" onclick="location.href='./reserve/done' ">予約する</button>
-            </form>
-            <!-- 予約情報表示 -->
-            <table class="reserve_confirmation">
-                <tr>
-                    <td class="reserve_confirmation_shop">Shop</td>
-                    <td class="reserve_confirmation_item_shop">{{ $shop['name'] }}</td>
-                </tr>
-                <tr>
-                    <td class="reserve_confirmation_title">Date</td>
-                    <td id="confirm-date" class="reserve_confirmation_item"></td>
-                </tr>
-                <tr>
-                    <td class="reserve_confirmation_title">Time</td>
-                    <td id="confirm-time" class="reserve_confirmation_item"></td>
-                </tr>
-                <tr>
-                    <td class="reserve_confirmation_number">Number</td>
-                    <td id="confirm-number" class="reserve_confirmation_item_number"></td>
-                </tr>
-            </table>
+        <!-- 画像 -->
+        <div class="image_content">
+            <img class="shop_image" src="{{ $shop['image_url'] }}">
         </div>
-
+        <!-- 分類 -->
+        <div class="classification">
+            <span>#{{ $shop['area']['name'] }}</span>
+            <span>#{{ $shop['genre']['name'] }}</span>
+        </div>
+        <!-- 説明 -->
+        <div class="classification">
+            <p>{{ $shop['description']}}</p>
+            <p class="review__information">全ての口コミ情報</p>
+        </div>
+        <!-- 口コミ表示 -->
+        @if( Auth::check() )
+        @if( is_null($my_review) )
+        <div class="review">
+            <a href="{{ url('/review/add/'.$shop['id']) }}" class="review_button_add">口コミを投稿する</a>
+        </div>
+        @endif
+        @endif
+        @foreach($reviews as $review)
+        @if($review->user_id == Auth::user()->id)
+        <hr>
+        <div class="button_flex">
+            <div class="review_button_flex">
+                <a href="{{ url('/review/edit/'.$shop['id']) }}" class="review__edit__button">口コミを編集</a>
+            </div>
+            <div class="review_button_flex">
+                <form method="POST" action="{{ url('/review/delete') }}">
+                    @csrf
+                    <input type="hidden" name="shop_id" value="{{ $shop['id'] }}">
+                    <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                    <button type="submit" class="review__delete__button">口コミを削除</button>
+                </form>
+            </div>
+        </div>
+        @endif
+        <div class="star">
+            @for ($counter = 0; $counter < 5; $counter++) <span class="{{ $counter < $review['star'] ? 'text-blue-600' : 'text-gray-200'}}">★</span>
+                @endfor
+        </div>
+        <p>{{$review['comment']}}</p>
+        <div class="image_content">
+            <img class="review_image" src="{{ $review['image_url'] }}" />
+        </div>
+        <hr>
+        @endforeach
     </div>
+    <!-- 予約 -->
+    <div class="reserve_content">
+        <h1 class="reserve">予約</h1>
+        <p class="reserve_text"></p>
+        @if (count($errors) > 0)
+        <p class='error_message'>入力に問題があります</p>
+        @endif
+        <form action="{{ route('reserve') }}" method="POST">
+            @csrf
+            <input type="hidden" name="shop_id" value="{{ $shop['id'] }}">
+            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <div class="date">
+                <input class="date" type="date" id="date" name="date" value="" min="{{ $tomorrow }}" />
+            </div>
+            @error('date')
+            <p class='error_message'>{{$message}}</p>
+            @enderror
 
-    @endsection
+            <!-- 時間のドロップダウン -->
+            <div class="drop_time">
+                <select name="time" class="drop_time" id="time">
+                    <optgroup label="lunch">
+                        <option value="" selected hidden>選択してください</option>
+                        <option value="11:00">11:00</option>
+                        <option value="11:30">11:30</option>
+                        <option value="12:00">12:00</option>
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                    </optgroup>
+                    <optgroup label="dinner">
+                        <option value="18:00">18:00</option>
+                        <option value="18:30">18:30</option>
+                        <option value="19:00">19:00</option>
+                        <option value="19:30">19:30</option>
+                        <option value="20:00">20:00</option>
+                    </optgroup>
+                </select>
+            </div>
+            @error('time')
+            <p class='error_message'>{{$message}}</p>
+            @enderror
+            <!-- 人数のドロップダウン -->
+            <div class="drop_number">
+                <select name="number" class="drop_number" id="number">
+                    <option value="" selected hidden>選択してください</option>
+                    <option value="1">1人</option>
+                    <option value="2">2人</option>
+                    <option value="3">3人</option>
+                    <option value="4">4人</option>
+                    <option value="5">5人</option>
+                    <option value="6">6人</option>
+                    <option value="7">7人</option>
+                    <option value="8">8人</option>
+                    <option value="9">9人</option>
+                    <option value="10">10人</option>
+                </select>
+            </div>
+            @error('number')
+            <p class='error_message'>{{$message}}</p>
+            @enderror
+
+            <button type="submit" class="reserve_button" onclick="location.href='./reserve/done' ">予約する</button>
+        </form>
+        <!-- 予約情報表示 -->
+        <table class="reserve_confirmation">
+            <tr>
+                <td class="reserve_confirmation_shop">Shop</td>
+                <td class="reserve_confirmation_item_shop">{{ $shop['name'] }}</td>
+            </tr>
+            <tr>
+                <td class="reserve_confirmation_title">Date</td>
+                <td id="confirm-date" class="reserve_confirmation_item"></td>
+            </tr>
+            <tr>
+                <td class="reserve_confirmation_title">Time</td>
+                <td id="confirm-time" class="reserve_confirmation_item"></td>
+            </tr>
+            <tr>
+                <td class="reserve_confirmation_number">Number</td>
+                <td id="confirm-number" class="reserve_confirmation_item_number"></td>
+            </tr>
+        </table>
+    </div>
+</div>
+@endsection
 </main>
